069bc3e528bb12b333ba72e29aa15fc5fb1ec167
Harsh Singh <your-email@example.com>
2026-07-02 13:46:46 +0530

    Frontend vercel

--- Frontend/src/Components/socket.js
+++ Frontend/src/Components/socket.js
@@ -1,3 +1,3 @@
 import { io } from "socket.io-client";
 
-export const socket = io("https://collaborative-coding-editor-queue.onrender.com");
+export const socket = io(import.meta.env.VITE_BACKEND_URL);
